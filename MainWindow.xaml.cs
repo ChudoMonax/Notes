992b6fb1e793d1e563bae98a9a2b2a931cc22829
ChudoMonax <tolya.gelman@live.com>
2022-06-16 15:04:10 +0700

    Added project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using Microsoft.Win32;
+using System.Windows.Controls;
+
+namespace Notes2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+		public MainWindow()
+		{
+			InitializeComponent();
+			cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+			cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+		}
+
+		private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
+		{
+			object temp = rtbEditor.Selection.GetPropertyValue(Inline.FontWeightProperty);
+			btnBold.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontWeights.Bold));
+			temp = rtbEditor.Selection.GetPropertyValue(Inline.FontStyleProperty);
+			btnItalic.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontStyles.Italic));
+			temp = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+			btnUnderline.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(TextDecorations.Underline));
+
+			temp = rtbEditor.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+			cmbFontFamily.SelectedItem = temp;
+			temp = rtbEditor.Selection.GetPropertyValue(Inline.FontSizeProperty);
+			cmbFontSize.Text = temp.ToString();
+		}
+
+		private void cmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (cmbFontFamily.SelectedItem != null)
+				rtbEditor.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, cmbFontFamily.SelectedItem);
+		}
+
+		private void cmbFontSize_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			rtbEditor.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontSize.Text);
+		}
+
+        private void NewFolder_Click(object sender, RoutedEventArgs e)
+        {
+			SaveNewFolder savenewfolder = new SaveNewFolder();
+			savenewfolder.ShowDialog();
+		}
+    }
+}
